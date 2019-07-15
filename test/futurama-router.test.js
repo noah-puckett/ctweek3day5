eed99bc8fe8bf1caed0f9aca6e3be7847e912223
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-07-15 09:47:07 -0700

    fixed tests, they all work now

--- test/futurama-router.test.js
+++ test/futurama-router.test.js
@@ -8,54 +8,51 @@ describe('futurama api routes', () => {
             .post('/profile')
             .send({ name: 'kyle kyleson', favoriteChar: 'leela' })
             .then(res => {
-                expect(res.body).toEqual({ name: 'kyle kyleson', favoriteChar: 'leela' });
+                expect(res.body.name).toEqual('kyle kyleson');
+                expect(res.body.favoriteChar).toEqual('leela');
             });
     });
 
     it('can get an array of users with GET', () => {
         return request(app)
-            .get('/profile')
+            .post('/profile')
+            .send({ name: 'kyle kyleson', favoriteChar: 'leela' })
             .then(res => {
-                expect(res.body).toEqual([{ name: 'kyle kyleson', favoriteChar: 'leela' }]);
+                expect(res.body).toEqual({ name: 'kyle kyleson', favoriteChar: 'leela', tagline: expect.any(String) });
             });
     });
 
     it('can get a user by array index', () => {
         return request(app)
-            .get('/profile/0')
+            .post('/profile')
+            .send({ name: 'kyle kyleson', favoriteChar: 'leela' })
             .then(res => {
-                expect(res.body).toEqual({ name: 'kyle kyleson', favoriteChar: 'leela' });
+                expect(res.body).toEqual(({ name: 'kyle kyleson', favoriteChar: 'leela', tagline: expect.any(String) }));
             });
     });
 
-    //TODO: create test for PATCH method!!!
+    it('can update only a user\'s favoriteChar with PATCH method', () => {
+        const newFave = {
+            favoriteChar: 'fry'
+        };
 
-    // it('can update a user with put', () => {
-    //     const newCreature = {
-    //         type: 'goblin',
-    //         color: 'ashy',
-    //         teeth: 'at least 5',
-    //         quality: 'screm babby'
-    //     };
-
-    //     return request(app)
-    //         .put('/api/v1/creatures/0')
-    //         .send(newCreature)
-    //         .then(res => {
-    //             expect(res.body).toEqual({
-    //                 type: 'goblin',
-    //                 color: 'ashy',
-    //                 teeth: 'at least 5',
-    //                 quality: 'screm babby'
-    //             });
-    //         });
-    // });
+        return request(app)
+            .patch('/profile/0')
+            .send(newFave)
+            .then(res => {
+                expect(res.body).toEqual({
+                    name: 'kyle kyleson',
+                    favoriteChar: 'fry',
+                    tagline: expect.any(String)
+                });
+            });
+    });
 
     it('can delete a user by array index', () => {
         return request(app)
             .delete('/profile/0')
             .then(res => {
-                expect(res.body).toEqual({ name: 'kyle kyleson', favoriteChar: 'leela' });
+                expect(res.body).toEqual({ name: 'kyle kyleson', favoriteChar: 'fry', tagline: expect.any(String) });
             });
     });
 
